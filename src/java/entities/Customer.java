aafec6a39d9332b7ce6df7c500e6f8441408d919
dimoDani <danidiazmoka@gmail.com>
2023-12-15 00:51:37 +0100

    First User side version

--- src/java/entities/Customer.java
+++ src/java/entities/Customer.java
@@ -1,9 +1,190 @@
 package entities;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
 /**
- *
- * @author Janam
+ * Represents a Customer entity, extending the User class and implementing Serializable.
+ * This class is mapped to the "customer" table in the "dindb" schema.
+ * The primary key is the email address, and it is used as a foreign key in the "tripsInfo" relationship.
  */
-public class Customer {
-    
+@Entity
+@Table(name = "customer", schema = "dindb")
+@PrimaryKeyJoinColumn(name = "mail")
+public class Customer extends User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The name of the customer.
+     */
+    private String name;
+
+    /**
+     * The zip code of the customer's address.
+     */
+    private Integer zip;
+
+    /**
+     * The address of the customer.
+     */
+    private String address;
+
+    /**
+     * The phone number of the customer.
+     */
+    private Integer phone;
+
+    /**
+     * A list of TripInfo objects associated with this customer.
+     */
+    @OneToMany(mappedBy = "customer")
+    private List<TripInfo> tripsInfo;
+
+    /**
+     * Default constructor for the Customer class.
+     */
+    public Customer() {
+    }
+
+    /**
+     * Retrieves the name of the customer.
+     *
+     * @return The name of the customer.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the customer.
+     *
+     * @param name The new name of the customer.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Retrieves the zip code of the customer's address.
+     *
+     * @return The zip code of the customer's address.
+     */
+    public Integer getZip() {
+        return zip;
+    }
+
+    /**
+     * Sets the zip code of the customer's address.
+     *
+     * @param zip The new zip code of the customer's address.
+     */
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
+
+    /**
+     * Retrieves the address of the customer.
+     *
+     * @return The address of the customer.
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Sets the address of the customer.
+     *
+     * @param address The new address of the customer.
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * Retrieves the phone number of the customer.
+     *
+     * @return The phone number of the customer.
+     */
+    public Integer getPhone() {
+        return phone;
+    }
+
+    /**
+     * Sets the phone number of the customer.
+     *
+     * @param phone The new phone number of the customer.
+     */
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * Retrieves the list of TripInfo objects associated with this customer.
+     *
+     * @return The list of TripInfo objects associated with this customer.
+     */
+    public List<TripInfo> getTripsInfo() {
+        return tripsInfo;
+    }
+
+    /**
+     * Sets the list of TripInfo objects associated with this customer.
+     *
+     * @param tripsInfo The new list of TripInfo objects associated with this customer.
+     */
+    public void setTripsInfo(List<TripInfo> tripsInfo) {
+        this.tripsInfo = tripsInfo;
+    }
+
+    /**
+     * Computes the hash code for this customer based on the email address.
+     *
+     * @return The hash code for this customer.
+     */
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (this.getMail() != null ? this.getMail().hashCode() : 0);
+        return hash;
+    }
+
+    /**
+     * Checks if this customer is equal to another object.
+     *
+     * @param obj The object to compare with this customer.
+     * @return true if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final Customer other = (Customer) obj;
+        return !((this.getMail() == null) ? (other.getMail() != null) : !this.getMail().equals(other.getMail()));
+    }
+
+    /**
+     * Generates a string representation of this customer.
+     *
+     * @return A string representation of this customer.
+     */
+    @Override
+    public String toString() {
+        return "Customer{"
+                + "email='" + getMail() + '\''
+                + ", name='" + name + '\''
+                + ", zip=" + zip
+                + ", address='" + address + '\''
+                + ", phone=" + phone
+                + '}';
+    }
 }
--- src/java/entities/User.java
+++ src/java/entities/User.java
@@ -1,9 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package entities;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
 /**
- *
- * @author Janam
+ * Entity JPA class for user data. The properties of this class are login , 
+ * name, profile and department.
+ * @author javi
  */
-public class User {
+@Entity
+@Table(name="user",schema="dindb")
+@NamedQueries({
+    @NamedQuery(name="findAllUsers",
+            query="SELECT u FROM User u ORDER BY u.name DESC"
+    ),
+    @NamedQuery(name="findUsersByEmail",
+            query="SELECT u FROM User u WHERE u.profile = :profile"
+    )
+})
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    /**
+     * Id field for user entity. It is also the mail id value for the user.
+     */
+    @Id
+    //@GeneratedValue(strategy = GenerationType.AUTO)->Not allowed for Hibernate
+    private String mail;
+    /**
+     * Full name of the user.
+     */
+    private String password;
+    /**
+     * {@link EnumUserType} value for the user.
+     */
+    @Enumerated(EnumType.ORDINAL)
+    private EnumUserType userType;
+    /**
+     * Gets mail value for user.
+     * @return The login value.
+     */
+    public String getMail() {
+        return mail;
+    }
+    /**
+     * Sets mail value for user.
+     * @param mail The login value.
+     */
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    /**
+     * Gets name value for user.
+     * @return The name value.
+     */
+    public String getPassword() {
+        return password;
+    }
+    /**
+     * Sets password value for user.
+     * @param password The name value.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    /**
+     * Gets Profile value for user.
+     * @return The Profile value.
+     */
+    public EnumUserType getUserType(){
+        return userType;
+    }
+    /**
+     * Sets Profile value for user.
+     * @param userType The Profile value.
+     */
+    public void setUserType(EnumUserType userType){
+        this.userType=userType;
+    }
+    /**
+     * HashCode method implementation for the entity.
+     * @return An integer value as hashcode for the object. 
+     */
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (mail != null ? mail.hashCode() : 0);
+        return hash;
+    }
+    /**
+     * This method compares two user entities for equality. This implementation
+     * compare login field value for equality.
+     * @param object The object to compare to.
+     * @return True if objects are equals, otherwise false.
+     */
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.mail == null && other.mail != null) || 
+            (this.mail != null && !this.mail.equals(other.mail))) {
+            return false;
+        }
+        return true;
+    }
+    /**
+     * This method returns a String representation for a user entity instance.
+     * @return The String representation for the user object. 
+     */
+    @Override
+    public String toString() {
+        return "javafxserverside.entity.User[ mail=" + mail + " ]";
+    }
     
 }
