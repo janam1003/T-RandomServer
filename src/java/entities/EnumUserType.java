ee6ff6d4625b2a84e8358bbc65067ee957c668c5
dimoDani <danidiazmoka@gmail.com>
2023-12-15 09:58:09 +0100

    Version 4 User side

--- src/java/entities/EnumUserType.java
+++ src/java/entities/EnumUserType.java
@@ -1,9 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package entities;
 
 /**
- *
- * @author Janam
+ * This enumeration is for users profiles. Includes the values USER and ADMIN
+ * @author danid
  */
 public enum EnumUserType {
-    
-}
+    /**
+     * The user is a customer.
+     */
+    CUSTOMER,
+    /**
+     * The user is an admin.
+     */
+    ADMIN;
+} 
